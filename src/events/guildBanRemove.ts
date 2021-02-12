27c5572ff32539d86a78bc50112f620dc5a3ed5f
nearlySplat <splatterxl@outlook.ie>
2021-02-12 12:34:58 +0000

    feat(logging): add guildBanRemove event

---
+++ src/events/guildBanRemove.ts
@@ -0,0 +1,47 @@
+import {
+  Client,
+  Guild,
+  GuildMember,
+  Permissions,
+  Snowflake,
+  TextChannel,
+  User,
+} from 'discord.js';
+import { createLogMessage } from '../util';
+
+export const execute = async (client: Client, guild: Guild, user: User) => {
+  if (!guild.me?.permissions.has(Permissions.FLAGS.VIEW_AUDIT_LOG)) return;
+  const channel = guild.channels.cache.find(
+      value =>
+        (value.name?.match(/^💡(-log(s|ging)?)?$/g) &&
+          value.type == 'text' &&
+          value
+            .permissionsFor(guild.me as GuildMember)
+            ?.has('SEND_MESSAGES')) ??
+        false
+    ) as TextChannel,
+    auditLogs = await guild.fetchAuditLogs(),
+    auditLogEntry = auditLogs.entries.find(
+      value =>
+        value.action == 'MEMBER_BAN_REMOVE' &&
+        (value.target as { id: Snowflake })?.id === user.id
+    );
+  if (!auditLogEntry) return false;
+  if (channel) {
+    const result = createLogMessage({
+      compact: channel.topic?.includes('--compact'),
+      victim: {
+        tag: user.tag,
+        id: user.id,
+      },
+      perpetrator: {
+        id: auditLogEntry?.executor.id,
+        tag: auditLogEntry?.executor.tag,
+      },
+      reason: auditLogEntry?.reason,
+      case: auditLogs.entries.size,
+      action: 'Unban',
+    });
+    channel.send(result);
+  }
+};
