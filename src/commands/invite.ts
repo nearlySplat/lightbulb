092e3563fab0c27a45d4b29fdbdc46741404253b
nearlySplat <splatterxl@outlook.ie>
2021-02-15 14:59:15 +0000

    fix: bruh

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -1,6 +1,6 @@
 import { CLIENT_COLOUR } from '../constants';
 import { CommandExecute, CommandMetadata } from '../types';
-import { Permissions } from "discord.js";
+import { Permissions, PermissionFlags } from "discord.js";
 
 export const execute: CommandExecute = ({ message, args }) => {
   message.reply(`<https://discord.com/oauth2/authorize?client_id=${args?.[0] ?? message.client.user?.id}&scope=bot${args[1] ? `&permissions=${args.slice(1).map(v => Permissions.FLAGS[v.toUpperCase() as keyof PermissionFlags] ?? 0).reduce((prev, curr) => prev + curr)}` : ""}>`, {
