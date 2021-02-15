47fc6b71faba77173a90250319982f77b3794d69
nearlySplat <splatterxl@outlook.ie>
2021-02-15 14:56:32 +0000

    feat(inviteCommand): add permissions calculator

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -1,8 +1,9 @@
 import { CLIENT_COLOUR } from '../constants';
 import { CommandExecute, CommandMetadata } from '../types';
+import { Permissions } from "discord.js";
 
 export const execute: CommandExecute = ({ message, args }) => {
-  message.reply(`<https://discord.com/oauth2/authorize?client_id=${args?.[0] ?? message.client.user?.id}&scope=bot>`, {
+  message.reply(`<https://discord.com/oauth2/authorize?client_id=${args?.[0] ?? message.client.user?.id}&scope=bot${args[1] ? `&permissions=${args.slice(1).map(v => Permissions.FLAGS[v.toUpperCase()] ?? 0).reduce((prev, curr) => prev + curr)}` : ""}>`, {
     allowedMentions: {
       repliedUser: false,
       parse: [],
