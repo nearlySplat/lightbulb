0449be2de50c6bcbc69c5b2c0104a3316b66d45b
nearlySplat <splatterxl@outlook.ie>
2021-02-16 15:58:25 +0000

    Update lookup.ts

--- src/commands/lookup.ts
+++ src/commands/lookup.ts
@@ -2,7 +2,7 @@ import { Context, CommandMetadata } from "../types";
 import { SnowflakeUtil } from "discord.js";
 
 export const execute = async (ctx: Context): Promise<boolean> => {
-  ctx.message.channel.send(Object.keys(SnowflakeUtil.deconstruct(ctx.args[0] ?? ctx.message.author.id)).map(([K, V]) => `**${K}**: \`${(V as (Date | string | number)) instanceof Date ? V.toLocaleString() : V}\``))
+  ctx.message.channel.send(Object.entries(SnowflakeUtil.deconstruct(ctx.args[0] ?? ctx.message.author.id)).map(([K, V]) => `**${K}**: \`${(V as (Date | string | number)) instanceof Date ? V.toLocaleString() : V}\``))
   return true;
 }
 
