a97711ef227dc06fd88b22564d31c1b35ac7c244
nearlySplat <splatterxl@outlook.ie>
2021-02-17 19:01:45 +0000

    Update base64.ts

--- src/commands/base64.ts
+++ src/commands/base64.ts
@@ -4,8 +4,8 @@ import { CommandExecute, CommandMetadata } from '../types';
 
 const invalidArg = arg => `__**Invalid Arguments!**__\n An argument for \`${arg}\` was \n- not specified or \n- not of the correct type.\n\nArguments for this command are: \`\`\`\n<action>: "encode" | "decode" = what you want me to do with your string\n<string>: string = the data you want me to encode/decode.\`\`\``
 export const execute: CommandExecute = ({ message, args }) => {
-  if (!args[0] || !["encode","decode"].includes(args[0])) return message.reply(invalidArg("action"));
-  if (!args[1]) return message.reply(invalidArg("data"));
+  if (!args[0] || !["encode","decode"].includes(args[0])) return message.reply(invalidArg("action")).then(() => false);
+  if (!args[1]) return message.reply(invalidArg("data")).then(() => false);
   const data = Buffer.from(args.slice(1).join(" "), args[0] == "decode" ? "base64" : undefined).toString(args[0] == "decode" ? "ascii" : "base64")
   const _ = new MessageEmbed()
     .setAuthor(`${args[0] == "decode" ? "Decoded" : "Encoded"} Base64 Text`)
