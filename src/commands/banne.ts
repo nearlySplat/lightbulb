087a12ec910234dca670b3f38bfaefd72a65241d
nearlySplat <splatterxl@outlook.ie>
2021-02-19 18:15:23 +0000

    feat: show the tag and not the id

--- src/commands/banne.ts
+++ src/commands/banne.ts
@@ -2,7 +2,7 @@ import { Context, CommandMetadata } from '../types';
 export const execute = ({ message, args }: Context): boolean | Promise<boolean> => {
   if (!args[0]) return message.reply("Who am I going to ~~call~~ banne? ~~Ghostbusters!~~ Nobody!").then(() => false);
   const target = message.guild?.members.cache.find(v => v.user.username.toLowerCase() == args.join(" ").toLowerCase() || v.user.id == args[0] || v.user.tag.toLowerCase() === args.join(" ").toLowerCase() || v.displayName.toLowerCase() == args.join(" ").toLowerCase()) ?? message.member;
-  message.reply(`***🔨 Successfully bent ${target.user.id}***`)
+  message.reply(`***🔨 Successfully bent ${target.user.tag}***`)
   return true;
 };
 
