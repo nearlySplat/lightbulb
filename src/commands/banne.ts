42e8eaf312307cf96bd36efe9b03e1cfd05562a1
nearlySplat <splatterxl@outlook.ie>
2021-02-19 18:11:05 +0000

    fix(typings&unusedVars)

--- src/commands/banne.ts
+++ src/commands/banne.ts
@@ -1,9 +1,7 @@
-import { MessageEmbed } from 'discord.js';
-import { CLIENT_COLOUR } from '../constants';
 import { Context, CommandMetadata } from '../types';
 export const execute = ({ message, args }: Context): boolean | Promise<boolean> => {
   if (!args[0]) return message.reply("Who am I going to ~~call~~ banne? ~~Ghostbusters!~~ Nobody!").then(() => false);
-  const target = message.guild.members.cache.find(v => v.name.toLowerCase() == args.join(" ").toLowerCase() || v.id == args[0] || v.tag.toLowerCase() === args.join(" ").toLowerCase()) ?? message.member;
+  const target = message.guild?.members.cache.find(v => v.name.toLowerCase() == args.join(" ").toLowerCase() || v.id == args[0] || v.tag.toLowerCase() === args.join(" ").toLowerCase()) ?? message.member;
   message.reply(`***🔨 Successfully bent ${target.user.id}***`)
   return true;
 };
