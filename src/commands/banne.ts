ed4a263bf51c9e77e9b1217c4406f8f985f1c629
nearlySplat <splatterxl@outlook.ie>
2021-02-20 12:18:44 +0000

    fix(banne): actually support IDs

--- src/commands/banne.ts
+++ src/commands/banne.ts
@@ -1,7 +1,7 @@
 import { Context, CommandMetadata } from '../types';
 export const execute = ({ message, args }: Context): boolean | Promise<boolean> => {
   if (!args[0]) return message.reply("Who am I going to ~~call~~ banne? ~~Ghostbusters!~~ Nobody!").then(() => false);
-  const target = message.guild?.members.cache.find(v => v.user.username.toLowerCase() == args.join(" ").toLowerCase() || v.user.id == args[0] || v.user.tag.toLowerCase() === args.join(" ").toLowerCase() || v.displayName.toLowerCase() == args.join(" ").toLowerCase()) ?? message.member;
+  const target = message.guild?.members.cache.get(args[0]?.replace(/(<@!?|>)/g, "")) ?? message.guild?.members.cache.find(v => v.user.username.toLowerCase() == args.join(" ").toLowerCase() || v.user.tag.toLowerCase() === args.join(" ").toLowerCase() || v.displayName.toLowerCase() == args.join(" ").toLowerCase()) ?? message.member;
   message.reply(`***🔨 Successfully bent ${target.user.tag}***`)
   return true;
 };
