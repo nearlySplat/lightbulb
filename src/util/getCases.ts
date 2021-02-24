8ff7a74ace4c5ccf10093a3f9c78b819958f1cc5
nearlySplat <splatterxl@outlook.ie>
2021-02-24 13:34:28 +0000

    Update getCases.ts

--- src/util/getCases.ts
+++ src/util/getCases.ts
@@ -1,6 +1,6 @@
 import { Guild } from "discord.js";
 
 export const getCases = async (guild: Guild) => {
-  const entries = [await guild.fetchAuditLogs({ type: "MEMBER_BAN_ADD" }), await guild.fetchAuditLogs({ type: "MEMBER_BAN_REMOVE" })];
-  return entries.reduce((a, b) => a + b.size)
+  const entries = [(await guild.fetchAuditLogs({ type: "MEMBER_BAN_ADD" })).entries.size, (await guild.fetchAuditLogs({ type: "MEMBER_BAN_REMOVE" })).entries.size];
+  return entries.reduce((a, b) => a + b)
 }
