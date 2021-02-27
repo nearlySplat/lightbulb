3321320c721da2a74023f076f6221df130baa48f
nearlySplat <splatterxl@outlook.ie>
2021-02-27 14:05:23 +0000

    feat: use i18n in banne command

--- src/commands/banne.ts
+++ src/commands/banne.ts
@@ -1,12 +1,13 @@
 import { GuildMember } from 'discord.js';
 import { Context, CommandMetadata } from '../types';
+import { get, interpolate } from '../util/i18n';
 export const execute = ({
   message,
   args,
 }: Context): boolean | Promise<boolean> => {
   if (!args[0])
     return message
-      .reply('Who am I going to ~~call~~ banne? ~~Ghostbusters!~~ Nobody!')
+      .reply(get('BANNE_NO_TARGET'))
       .then(() => false);
   const target: GuildMember =
     message.guild?.members.cache.get(args[0]?.replace(/(<@!?|>)/g, '')) ??
@@ -17,7 +18,7 @@ export const execute = ({
         v.displayName.toLowerCase() == args.join(' ').toLowerCase()
     ) ??
     message.member;
-  message.reply(`***🔨 Successfully bent ${target.user.tag}***`);
+  message.reply(interpolate(get('BANNE_SUCCESSFUL'), { target: target.user.tag }));
   return true;
 };
 
