19dfe4dbc6dac488a7c9b35f097529a3c80b6ae3
nearlySplat <splatterxl@outlook.ie>
2021-02-28 20:11:08 +0000

    i18n: add uwu language

---
+++ src/util/i18n/uwu/strings.ts
@@ -0,0 +1,30 @@
+export const ABOUT_LONG_DESCRIPTION = `👋 Hewwo >~<! I'm 🤖💡, the [TypeScript](https://typescriptlang.org) rewrite of Eureka!
+
+    **My features**:
+    - Logging
+    - Logging
+    - Logging
+
+    Aaand... that's about it, senpai! All you need to be my fwend is a ✨ special ✨ text channel called \`💡\`! Actually, the weal uwu programmer wegex is \`/^💡(-log(s|ging)?)?$/g\`, but dat cwazy stuff is weiwd.
+
+    **What I log:**
+    - 🔨 Bans
+    - 🔧 Unbans
+    - 👢 Kicks (Coming Soon)`.replace(/\n +/g, '\n');
+export const ABOUT_HEADER = 'About Me';
+export const BANNE_SUCCESSFUL = '***🔨 Successfully made {{target}} leave ;~;***';
+export const BANNE_NO_TARGET =
+  'How could you do this to yourself? ;w;';
+export const I18N_KEY_NOT_FOUND = "Oh noes! {{key}} has not been uwu-ified!";
+export const GENERIC_REQUESTED_BY = '{{requester}} is special and used me!';
+export const BEAN_NO_TARGET = 'here you go, some fresh beans!';
+export const BEAN_SUCCESSFUL = '***<:bean:813134247505559572> gave some beans to {{target}}***'
+export const DIE_SUCCESS = "bye, senpai... ;;";
+export const HEX_HEADER = "Hexadecimal Color #{{color}}";
+export const HEX_BODY = `**Hexadecimal Value**: #{{hex_value}}
+                         **Decimal Value**: {{decimal_value}}`.replace(/\n +/g, "\n");
+export const PURGE_HELP_BODY = `There are many features in this command.
+                                - \`purge bots [amount]\`: deletes the last 100 messages that are by bots and are under 14 days old. Optionally takes a second argument for an amount of messages to delete.
+                                - \`purge regexp <regexp>\`: deletes messages matching that RegExp.
+                               `.replace(/\n +/g, '\n')
+export const PURGE_HELP_HEADER = "Purge Help"
