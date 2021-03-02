fa0ab1e2eb5a7d5c04ff2c57ad8d4ec346e3e992
nearlySplat <splatterxl@outlook.ie>
2021-03-02 14:02:55 +0000

    merge: i18n-migration...development (#8)
    * i18n: add GENERIC_REQUESTED_BY to en_UK strings
    
    * feat: update help command to be the vaguest possible

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,34 +2,10 @@ import { Collection, GuildMember, MessageEmbed, ClientUser } from 'discord.js';
 import { CLIENT_COLOUR } from '../constants';
 import { CommandExecute, CommandMetadata } from '../types';
 import { getAccessLevel, getCurrentLevel } from '../util';
-import { get, interpolate } from "../util/i18n";
+import { get } from '../util/i18n';
 
-export const execute: CommandExecute = ({ message, commands, locale }) => {
-  const _ = new MessageEmbed()
-    .setAuthor('Help')
-    .setColor(CLIENT_COLOUR)
-    .setFooter(
-      interpolate(get("GENERIC_REQUESTED_BY", locale), { requester: `${message.author.tag} (${message.author.id})` }),
-      message.author.avatarURL() as string
-    )
-    .setDescription(
-      (commands.filter(
-        v =>
-          !!v.meta &&
-          getAccessLevel(v.meta.accessLevel as 0 | 1 | 2 | 3) <=
-            getCurrentLevel(message.member as GuildMember) && !v.meta.hidden
-      ) as Collection<
-        string,
-        { execute: CommandExecute; meta: CommandMetadata }
-      >)
-        .map(value => `**${value.meta.name}** - ${value.meta.description}`)
-        .join('\n')
-    )
-    .setThumbnail((message.client.user as ClientUser).avatarURL() as string)
-    .setTimestamp();
-  message.reply({
-    embed: _,
-  });
+export const execute: CommandExecute = ({ message, commands }) => {
+  message.channel.send(get("HELP_ARRIVED"))
   return true;
 };
 
