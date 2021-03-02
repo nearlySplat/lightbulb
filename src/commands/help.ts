349930178f909871c82416a273f894d73445f972
nearlySplat <splatterxl@outlook.ie>
2021-03-02 13:59:20 +0000

    feat: update help command to be the vaguest possible

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,37 +2,10 @@ import { Collection, GuildMember, MessageEmbed, ClientUser } from 'discord.js';
 import { CLIENT_COLOUR } from '../constants';
 import { CommandExecute, CommandMetadata } from '../types';
 import { getAccessLevel, getCurrentLevel } from '../util';
+import { get } from '../util/i18n';
 
 export const execute: CommandExecute = ({ message, commands }) => {
-  const _ = new MessageEmbed()
-    .setAuthor('Help')
-    .setColor(CLIENT_COLOUR)
-    .setFooter(
-      `Requested by ${message.author.tag} (${message.author.id})`,
-      message.author.avatarURL() as string
-    )
-    .setDescription(
-      (commands.filter(
-        v =>
-          !!v.meta &&
-          getAccessLevel(v.meta.accessLevel as 0 | 1 | 2 | 3) <=
-            getCurrentLevel(message.member as GuildMember) && !v.meta.hidden
-      ) as Collection<
-        string,
-        { execute: CommandExecute; meta: CommandMetadata }
-      >)
-        .map(value => `**${value.meta.name}** - ${value.meta.description}`)
-        .join('\n')
-    )
-    .setThumbnail((message.client.user as ClientUser).avatarURL() as string)
-    .setTimestamp();
-  message.reply({
-    allowedMentions: {
-      repliedUser: false,
-      parse: [],
-    },
-    embed: _,
-  });
+  message.channel.send(get("HELP_ARRIVED"))
   return true;
 };
 
