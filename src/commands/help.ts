c4e9b8fe1c7382353a106ffa9a97c6f504a54b91
nearlySplat <splatterxl@outlook.ie>
2021-03-03 11:43:59 +0000

    fix: I'm probably never going to localise command descriptions

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -6,7 +6,7 @@ import { get } from '../util/i18n';
 
 export const execute: CommandExecute = ({ message, locale, commands, args }) => {
   if (!args[0]) message.channel.send(get("HELP_ARRIVED", locale));
-  else message.channel.send(get(`${commands.has(args[0]) ? (commands.get(args[0])?.meta.name.toUpperCase() + "_DESCRIPTION") : "HELP_ARRIVED"}`, locale))
+  else message.channel.send(commands.has(args[0]) ? commands.get(args[0]).meta?.description : get("HELP_ARRIVED", locale))
   return true;
 };
 
