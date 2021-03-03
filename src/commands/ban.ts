e39a3d639d5bade46e97b5b3071689ba4e4e0ade
nearlySplat <splatterxl@outlook.ie>
2021-03-03 19:54:56 +0000

    fix: member was still being referenced

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -2,7 +2,7 @@ import { Permissions } from "discord.js";
 import { CommandExecute, CommandMetadata } from "../types";
 import { getCases } from "../util";
 import { get, interpolate } from "../util/i18n";
-import { ERROR_CODES } from "../constants";
+import { ERROR_CODES, WHITELIST } from "../constants";
 export const execute: CommandExecute = async ({ message, args, locale }) => {
   if (!args[0]) return false;
   if (!message.guild.me.permissions.has(Permissions.FLAGS.BAN_MEMBERS))
@@ -36,16 +36,17 @@ export const execute: CommandExecute = async ({ message, args, locale }) => {
   if (!member) {
     ban();
   }
-  if (member.user.id === message.guild.ownerID) {
+  if (member?.user.id === message.guild.ownerID) {
     message.channel.send(interpolate(get("GENERIC_ERROR", locale), { code: ERROR_CODES.TARGET_IS_OWNER, message: "Target is owner" }));
     return false;
   }
-  if (member.user.id === message.client.user?.id) {
+  if (member?.user.id === message.client.user?.id || WHITELIST.includes(target.id)) {
     message.channel.send(interpolate(get("GENERIC_ERROR", locale), { code: ERROR_CODES.DISALLOWED_TARGET, message: "Disallowed target" }))
     return false;
   }
-  if (member.user.id === message.author.id) {
-    
+  if (member?.user.id === message.author.id) {
+    message.channel.send(interpolate(get("GENERIC_ERROR", locale), { code: ERROR_CODES.SELF_IS_MODERATION_TARGET, message: "Self is moderation target" }))
+    return false;
   }
   if (member)
     if (
