94eb1bd3c66eb58d3235c0d854fed824fb9ae9f2
nearlySplat <splatterxl@outlook.ie>
2021-03-28 22:37:21 +0100

    fix: add modified lines

--- scripts/licenser.fish
+++ scripts/licenser.fish
@@ -4,5 +4,6 @@ for file in src/**/*.ts
 
     if not grep -q Copyright "$file"
       cat assets/licenseHeader.txt "$file" > "$file.licensed" && mv "$file.licensed" "$file"
+      git add $file
     end
 end
