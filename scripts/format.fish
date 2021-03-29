85ca2ba74190d7fb496aca7312141648ce9f39e6
nearlySplat <splatterxl@outlook.ie>
2021-03-29 18:46:55 +0100

    test(formatting|husky): fix formatting script

--- scripts/format.fish
+++ scripts/format.fish
@@ -1,7 +1,6 @@
 echo "Looking for unformatted source files..."
 set -l files (prettier -l **/*.{json,ts} *.{json,ts})
-echo $files
-if test -z $files
+if test -z "$files"
 	echo "No unformatted files found."
 else
 	echo "Oh no! Some files are unformatted!"
