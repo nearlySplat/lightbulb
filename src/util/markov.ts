9799a2371ec0aecfa7609dbd4f1d96111532c1c6
nearlySplat <splatterxl@outlook.ie>
2021-04-20 20:07:38 +0100

    refactor: markov refactor

---
+++ src/util/markov.ts
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2020 Splaterxl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+export class Markov {
+  public static SENTENCE_BOUNDARIES = /\b[!?.\n]+\b/gi;
+  public static WORD_JOINERS = /[-:;,@\$%^&*!?.€£¥₩+\-\/\\=#]/g;
+  public static WORD = /(\b\w+\b([-:;,@\$%^&*!?.€£¥₩+\-\/\\=#)\][}{]\b\w*\b)*)/gi;
+  public static WORD_MATCH = (word: string) =>
+    new RegExp(`\\b${word}\\b(${this.WORD_JOINERS.source}\\b\\w*\\b)?`, 'gi');
+  public static SENTENCE = new RegExp(
+    `${this.WORD.source}+${this.SENTENCE_BOUNDARIES.source}`,
+    'gi'
+  );
+  public static TUPLE_WORD_MATCH = (existing: RegExp) =>
+    new RegExp(`${existing.source}\\s+${this.WORD.source}`, 'gi');
+  constructor(arr?: string | string[]) {
+    if (arr) this.seed(arr);
+  }
+  public static from(arr: string | string[]) {
+    return new Markov(arr);
+  }
+  public seed(arr: string | string[]) {
+    if (!Array.isArray(arr)) arr = arr.match(Markov.SENTENCE);
+    this.sentences = arr;
+    this._splittedSentences = this.sentences.map(value => value.split(/\s+/));
+    for (const [sentence, words] of this.sentences.map<[string, string[]]>(
+      value => [value, value.match(Markov.WORD) ?? []]
+    )) {
+      for (const word of words) {
+        const matches = (
+          sentence.match(Markov.TUPLE_WORD_MATCH(Markov.WORD_MATCH(word))) ?? []
+        )
+          .filter(v => v)
+          .map(v => v.split(/\s+/).reverse()[0]);
+        let already = this.matches.get(word);
+        if (already) already.push(...matches);
+        else already = matches;
+        already = [...new Set(already)];
+        this.matches.set(word, already);
+      }
+    }
+    this.startingWords = this._splittedSentences.map(value => value[0]);
+    this.endingWords = this._splittedSentences.map(value => value.reverse()[0]);
+    return this;
+  }
+  public generate(length = 0, options: MarkovTypes.GenerateOptions = {}) {
+    let text = options.starting
+      ? options.starting
+      : options.ending
+      ? options.ending
+      : this.getRandomStartingWord();
+    let word = text;
+    let finished = false;
+    const hasToHaveRegExp: typeof options.hasToHave extends string
+      ? RegExp
+      : null = options.hasToHave ? Markov.WORD_MATCH(options.hasToHave) : null;
+    do {
+      for (; !finished; ) {
+        let matched: string = this.matches.get(word) as any;
+        console.log('current sentence: ', text, ' and next match: ', matched);
+        matched = matched?.[Math.floor(Math.random() * matched.length)];
+        if (!matched || !matched.length) {
+          finished = true;
+          break;
+        }
+        if (matched?.toLowerCase() === word.toLowerCase()) {
+          finished = true;
+          break;
+        }
+        text += ' ' + matched;
+        word = matched;
+      }
+    } while (
+      length
+        ? text.split(/\s+/).length < length
+        : options.hasToHave
+        ? !hasToHaveRegExp.test(text)
+        : !finished
+    );
+    return text;
+  }
+  private _splittedSentences = new Array<string[]>();
+  public startingWords = new Array<string>();
+  public endingWords = new Array<string>();
+  public sentences = new Array<string>();
+  public matches = new Map<string, string[]>();
+  public getRandomStartingWord() {
+    return this.startingWords[
+      Math.floor(Math.random() * this.startingWords.length)
+    ];
+  }
+}
+
+namespace MarkovTypes {
+  export interface GenerateOptions {
+    hasToHave?: MarkovTypes.Matchable<typeof Markov.WORD>;
+    length?: number;
+    starting?: MarkovTypes.Matchable<typeof Markov.WORD>;
+    ending?: MarkovTypes.Matchable<typeof Markov.WORD>;
+  }
+  export type Matchable<_T extends RegExp, M = string> = M;
+}
