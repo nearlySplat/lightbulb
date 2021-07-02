2f10f07221dee3ab1fd4984deb935e6548a92a23
nearlySplat <splatterxl@outlook.ie>
2021-07-03 00:31:35 +0100

    Update deploy.ts

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -3,6 +3,7 @@ import { sleep } from '../util/';
 import { WHITELIST } from '../constants';
 import { MessageActionRow, MessageButton } from 'discord.js';
 import { buttonHandlers } from '../events/message';
+import path from "path";
 const nerrix = '332864061496623104';
 export const meta: CommandMetadata = {
   name: 'deploy',
@@ -36,7 +37,7 @@ export const execute: CommandExecute = async ctx => {
       });
       coll.on('collect', async interaction => {
         coll.stop();
-        await interaction.reply("Obama has been deployed.");
+        await interaction.reply({ content: "Obama has been deployed.", attachments: ['1.png', '2.jpeg', '3.jpeg', 'final.png'].map(v => path.resolve(__dirname, '..', '..', 'assets', 'obama', v))});
         await msg.edit({ components: [], content: "Deployment has been authorized." });
         if (interaction.customID === 'a') r(true);
         else r(false);
