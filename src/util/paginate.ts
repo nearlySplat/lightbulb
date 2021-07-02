8238feaf996661f7095b404b2f67c948d4e3aaa3
Splatterxl <splatterxl@outlook.ie>
2021-07-02 14:28:07 +0100

    feat: add pagination functuon

--- src/util/paginate.ts
+++ src/util/paginate.ts
@@ -1,17 +1,87 @@
-/*
- * Copyright (C) 2020 Splatterxl
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-// coming soon
+// Copyright not applicable
+import {
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed,
+  MessageSelectMenu,
+  Snowflake,
+} from 'discord.js';
+import { defaultDeleteButton } from '../events/message.js';
+import { ButtonInteractionHandler, CommandResponse } from '../types.js';
+// written by bread, ported to ts by Splatterxl
+export async function pagination(
+  pages: {
+    embed: MessageEmbed;
+    label: string;
+    description: string;
+    emoji: string;
+  }[],
+  user: Snowflake
+): Promise<CommandResponse> {
+  let i = 0;
+  const components = (disabled = false) => [
+    new MessageActionRow().addComponents(
+      defaultDeleteButton[0].components[0].setDisabled(disabled),
+      new MessageButton()
+        .setCustomID('p_prev')
+        .setStyle('PRIMARY')
+        .setEmoji('cutie_backward:848237448269135924')
+        .setDisabled(pages.length === 1 || i === 0 || disabled),
+      new MessageButton()
+        .setCustomID('p_stop')
+        .setStyle('DANGER')
+        .setEmoji('cutie_stop:848633645123371038')
+        .setDisabled(disabled),
+      new MessageButton()
+        .setCustomID('p_next')
+        .setStyle('PRIMARY')
+        .setEmoji('cutie_forward:848237230363246612')
+        .setDisabled(pages.length === 1 || i === pages.length - 1 || disabled)
+    ),
+    new MessageActionRow().addComponents(
+      new MessageSelectMenu()
+        .addOptions(
+          pages.map((v, i) => {
+            delete v.embed;
+            return { ...v, value: `${i}` };
+          })
+        )
+        .setDisabled(disabled)
+        .setCustomID('p_page')
+    ),
+  ];
+  const handle: ButtonInteractionHandler = ctx => {
+    if (ctx.user.id !== user) return { type: 6 };
+    switch (ctx.customID) {
+      case 'internal__delete':
+        ctx.message.delete();
+        ctx.removeListener();
+        break;
+      case 'p_stop':
+        ctx.message.edit({ components: components(true) });
+        break;
+      case 'p_prev':
+        ctx.message.edit({
+          embeds: [pages[--i].embed],
+          components: components(),
+        });
+        break;
+      case 'p_next':
+        ctx.message.edit({
+          embeds: [pages[++i].embed],
+          components: components(),
+        });
+        break;
+      case 'p_page':
+        i = +ctx.interaction.data.values[0];
+        console.log(ctx.interaction.data, i, !!pages[i]);
+        ctx.message.edit({
+          embeds: [pages[i].embed],
+          components: components(),
+        });
+        break;
+    }
+    return { type: 6 };
+  };
+  return [{ embed: pages[i].embed, components: components() }, handle];
+}
