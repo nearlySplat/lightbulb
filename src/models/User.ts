4f223bb319a1fded3c3bfa036b16e220c65a08c6
Splatterxl <splatterxl@outlook.ie>
2021-07-04 14:24:08 +0100

    add user
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /*
  * Copyright (C) 2020 Splatterxl
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
- import { Schema } from 'mongoose';
-const UserSchema = new Schema('User', {
-  id: String,
+import { model, Schema } from 'mongoose';
+const schemaData = {
+  achievements: [Number],
   commands: [String],
-  achievements: [String],
-});
+  uid: String,
+  pronouns: {
+    subject: String,
+    object: String,
+    possessiveDeterminer: String,
+    posessivePronoun: String,
+    singularOrPlural: String,
+  },
+};
+const schema = new Schema(schemaData);
+export const User = model<typeof schemaData>('User', schema);
