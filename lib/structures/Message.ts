a0c69f6cca9ee10cf8016450296be169b21913b5
Splatterxl <splatterxl@outlook.ie>
2021-07-12 14:28:05 +0100

    fix typings for Message#reply implement custom additions to reply function

--- lib/structures/Message.ts
+++ lib/structures/Message.ts
@@ -1,6 +1,18 @@
-import { Message as DJSMessage } from 'discord.js';
+import { Message as DJSMessage, MessageOptions } from 'discord.js';
+import { ExtendedMessageOptions } from '../../src/types.js';
 import { Candle } from './Client.js';
 
 export class Message extends DJSMessage {
   declare client: Candle;
+  async reply(options: ExtendedMessageOptions): Promise<Message> {
+    if (options.embeds || options.embed) {
+      options.embeds = [options.embed];
+    }
+    if (options.code) {
+      options.content = `\`\`\`${options.code === true ? '' : options.code}\n${
+        options.content
+      }\n\`\`\``;
+    }
+    return <Promise<Message>>super.reply(<MessageOptions>options);
+  }
 }
