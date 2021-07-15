de3668cb704a10ba3e9bce1b6641104a77eac2e2
Splatterxl <splatterxl@outlook.ie>
2021-07-15 21:23:04 +0100

    add deployer
    h

---
+++ deployr.js
@@ -0,0 +1,36 @@
+const child_process = require('child_process');
+const app = require('express')();
+const conf = require('./deployrconf.json');
+const { WebhookClient } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const { inspect } = require('util');
+
+app.listen(8085);
+
+app.post('/', async (req, res) => {
+  if (req.header('user-agent') !== conf.auth)
+    return res.status(403).send('unauthorized');
+  try {
+    child_process.execSync('git pull');
+    child_process.exec(
+      'tsc && DEPLOY=1 pm2 restart 0 --update-env',
+      (e, o, er) => {
+        console.log(inspect(e), inspect(o), inspect(er));
+      }
+    );
+    const client = new WebhookClient(conf.webhook.id, conf.webhook.token);
+    await client.send({
+      embed: new MessageEmbed()
+        .setDescription(
+          'deployed on commit' +
+            child_process.execSync("git log -n 1 --format='%H'")
+        )
+        .setColor('YELLOW'),
+      username: 'Lightbulb',
+      avatarURL: conf.webhook.avatar,
+    });
+    return res.status(200).send('success');
+  } catch (e) {
+    return res.status(500).send(inspect(e));
+  }
+});
